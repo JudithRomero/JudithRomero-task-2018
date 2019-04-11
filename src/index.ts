f2d3bf5d10bbd46f51a4cdc74b6743cfaf23dd26
[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅] <(-).*>
2019-04-11 23:18:31 +0500

    dockerfile for heroku

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,15 @@ import { init, connect } from './models'
     console.error('DB_STRING not specified!')
     return
   }
+  const port = parseInt(String(process.env.PORT), 10)
+  if (Number.isNaN(port)) {
+    console.error('PORT is not a number!')
+    return
+  }
   const models = await connect(process.env.DB_STRING)
   console.log('connected to db')
   await init(models)
   console.log('initialized db')
   const app = await getApp(models)
-  app.listen(3000, () => console.log('Example app listening on port 3000!'))
+  app.listen(port, () => console.log(`app listening on port ${port}!`))
 })().catch(console.error)
